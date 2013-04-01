171eb51507d5a12078dde7fd4bae3dbad8098ddc
Agaricus <agaricusb@yahoo.com>
2013-03-31 20:50:13 -0700

    Dump block IDs and item IDs

--- src/main/java/agaricus/mods/modanalyzer/ModAnalyzer.java
+++ src/main/java/agaricus/mods/modanalyzer/ModAnalyzer.java
@@ -8,6 +8,8 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 
 import java.util.logging.Level;
 
@@ -26,6 +28,22 @@ public class ModAnalyzer {
     @PostInit
     public void postInit(FMLPostInitializationEvent event) {
         FMLLog.log(Level.FINE, "Loading ModAnalyzer...");
+
+        for (int i = 0; i < Block.blocksList.length; ++i) {
+            Block block = Block.blocksList[i];
+            if (block != null) {
+                System.out.println("Block,"+block.blockID+","+block.getUnlocalizedName()+","+block.getLocalizedName()+","+block.blockHardness+","+block.blockMaterial+","+block.blockParticleGravity+","+block.blockResistance);
+            }
+        }
+
+        for (int i = 0; i < Item.itemsList.length; ++i) {
+            Item item = Item.itemsList[i];
+            if (item != null) {
+                System.out.println("Item,"+item.itemID+","+item.getUnlocalizedName()+","+item.getHasSubtypes()+","+item.getPotionEffect()+","+item.getItemEnchantability()+","+item.getItemStackLimit());
+            }
+        }
+
+        Runtime.getRuntime().halt(0);
     }
 }
 
