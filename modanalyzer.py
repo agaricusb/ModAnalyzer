06847148d07e89bf19044b43acdecc2c238735d7
Agaricus <agaricusb@yahoo.com>
2013-03-31 20:39:56 -0700

    Run server for each mod

--- modanalyzer.py
+++ modanalyzer.py
@@ -5,8 +5,11 @@ FORGE_VERSION = "7.7.1.624"
 
 TEST_SERVER_ROOT = "temp-server"
 TEST_SERVER_FILE = "minecraft_server+forge.jar"
+TEST_SERVER_CMD = "java -mx2G -jar %s"
 
-import os, urllib, zipfile, urllib2, tempfile
+ALL_MODS_DIR = "allmods"
+
+import os, urllib, zipfile, urllib2, tempfile, shutil
 
 def setupServer(serverFilename):
     mcZip = getURLZip("http://assets.minecraft.net/%s/minecraft_server.jar" % (MC_VERSION.replace(".", "_"),))
@@ -27,7 +30,7 @@ def setupServer(serverFilename):
             # overwrites - Forge > vanilla
             serverZip.writestr(member, forgeZip.read(member))
             print "O",member
-    print "Server at",serverFilename
+    print "Server setup at",serverFilename
 
 def getURLZip(url):
     f = tempfile.mktemp()
@@ -35,14 +38,44 @@ def getURLZip(url):
     urllib.urlretrieve(url, f)
     return zipfile.ZipFile(f, 'r')
 
+def runServer():
+    d = os.getcwd()
+    os.chdir(TEST_SERVER_ROOT)
+    os.system(TEST_SERVER_CMD % (TEST_SERVER_FILE,))
+    os.chdir(d)
+
+
+def analyzeMod(fn):
+    modsFolder = os.path.join(TEST_SERVER_ROOT, "mods")
+    if os.path.exists(modsFolder):
+        shutil.rmtree(modsFolder)
+    os.mkdir(modsFolder)
+
+    shutil.copyfile(fn, os.path.join(modsFolder, os.path.basename(fn)))
+
+    runServer()
+
+    raise SystemExit
+
+def getMods():
+    if not os.path.exists(ALL_MODS_DIR):
+        os.mkdir(ALL_MODS_DIR)
+    mods = []
+    for m in sorted(os.listdir(ALL_MODS_DIR)):
+        if m.startswith("."): continue
+        mods.append(os.path.join(ALL_MODS_DIR, m))
+    return mods
 
 def main():
     server = os.path.join(TEST_SERVER_ROOT, TEST_SERVER_FILE)
     if not os.path.exists(server):
         if not os.path.exists(TEST_SERVER_ROOT):
             os.mkdir(TEST_SERVER_ROOT)
-
         setupServer(server)
+    print "Using server at:",server
+    for mod in getMods():
+        analyzeMod(mod)
+
 
 if __name__ == "__main__":
     main()
