513c457bfee4d7b86156a39d82d0e24f5dffc7d7
Agaricus <agaricusb@yahoo.com>
2013-04-06 18:36:20 -0700

    Show IDs list across all mods, to help resolve conflicts
    Example:
    https://gist.github.com/agaricusb/5328473

--- list.py
+++ list.py
@@ -6,6 +6,7 @@ import pprint
 
 DATA_DIR = "data"
 
+"""Load content into dict keyed mod name -> kind -> id -> key/value."""
 def load():
     contents = {}
     for filename in os.listdir(DATA_DIR):
@@ -31,10 +32,50 @@ def load():
 
     return contents
 
+
+"""Given a kind, get id -> mod -> key/value. Used for showing conflicts."""
+def sliceAcross(contents, kind):
+    sliced = {}
+    for mod, content in contents.iteritems():
+        for id, data in content.get(kind, {}).iteritems():
+            id = intIfInt(id)
+
+            if not sliced.has_key(id):
+                sliced[id] = {}
+
+            sliced[id][mod] = data
+    return sliced
+
+"""Get n as an integer, if it can be parsed as an integer."""
+def intIfInt(n):
+    try:
+        return int(n)
+    except:
+        return n
+
+def getKinds(contents):
+    kinds = set()
+    for mod, content in contents.iteritems():
+        for kind in content.keys():
+            kinds.add(kind)
+    return kinds
+
+def showSlice(sliced):
+    for id in sorted(sliced.keys()):
+        mods = sliced[id].keys()
+        print id, "\t", "\t".join(mods)
+
 def main():
     content = load()
 
-    print json.dumps(content, sort_keys=True, indent=1)
+    for kind in getKinds(content):
+        print
+        print "*" * (len(kind) + 4)
+        print "*", kind, "*"
+        print "*" * (len(kind) + 4)
+        showSlice(sliceAcross(content, kind))
+
+    #print json.dumps(content, sort_keys=True, indent=1)
 
 if __name__ == "__main__":
     main()
